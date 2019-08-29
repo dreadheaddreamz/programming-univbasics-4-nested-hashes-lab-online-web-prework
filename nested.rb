a52b5724d4d0390d53de0fd65a719fc2b0986f13
James Batsell <jamesbatsell05@gmail.com>
2019-08-29 01:16:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,9 +64,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash.upshift[:yukihiro_matsumoto][:known_for] = "Ruby"[:languages] = ["LISP","C"]
-	
-	programmer_hash = 
+
+		programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
